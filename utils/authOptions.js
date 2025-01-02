18cb659596410d368849b561b15cc4cc6d0a94d5
Shukra Tamang <shukratamang.npl@gmail.com>
2025-01-02 15:48:19 +1100

    Next Auth and Provider Setup

---
+++ utils/authOptions.js
@@ -0,0 +1,26 @@
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    // Invoked on successful sign in
+    async signIn({ profile }) {
+      // Connect to the database
+      // Check if user exists
+      // if not, create user
+      // Return true to allow sign in
+    },
+  },
+};
