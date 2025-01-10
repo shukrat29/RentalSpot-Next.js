4d511c22949ef8305324edf998395ab15500f2a7
Shukra Tamang <shukratamang.npl@gmail.com>
2025-01-10 20:15:00 +1100

    added mobile login with google

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -262,8 +262,17 @@ const Navbar = () => {
             )}
             {!session && (
               <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5">
-                <i className="fa-brands fa-google mr-2"></i>
-                <span>Login or Register</span>
+                {providers &&
+                  Object.values(providers).map((provider, index) => (
+                    <button
+                      key={index}
+                      onClick={() => signIn(provider.id)}
+                      className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                    >
+                      <FaGoogle className="text-red-600 mr-2" />
+                      <span>Login or Register</span>
+                    </button>
+                  ))}
               </button>
             )}
           </div>
